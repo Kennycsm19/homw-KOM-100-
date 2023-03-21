5ab2bb86710ba3c14e731a409fa6dbe66070891d
Kennycsm19 <112910447+Kennycsm19@users.noreply.github.com>
2023-03-21 18:42:40 -0500

    Add files via upload

---
+++ KOM/estructura/footer.php
@@ -0,0 +1,75 @@
+<footer>
+  <div class="footer">
+    <div class="lastinfo">
+      <div class="logolast"></div>
+      <p>Agencia digital especializada en ayudarte a conseguir clientes e incrementar tus ingresos con páginas optimizadas con SEO para lograr un retorno rápido de tu inversión.</p>
+    </div>
+    <div class="reds">
+      <a href="#"></a>
+      <a href="#"></a>
+      <a href="#"></a>
+      <a href="#"></a>
+    </div>
+    <div class="legal">
+      <a href=“#”>Términos y Condiciones</a>
+      <a href=“#”>Términos del Servicio</a>
+      <a href=“#”>Política de Privacidad</a>
+      <a href=“#”>Libro de Reclamaciones</a>
+    </div>
+    <div class="listfooone">
+      <p>KOM ‧ Agency</p>
+      <ul>
+        <li><span></span><a href=“#”>La Agencia</a></li>
+        <li><span></span><a href=“#”>Portafolio</a></li>
+        <li><span></span><a href=“#”>Academia Digital</a></li>
+        <li><span></span><a href=“#”>Posición Ø | SEO Agency</a></li>
+        <li><span></span><a href=“#”>Trabaja en KOM</a></li>
+      </ul>
+    </div>
+    <div class="listfootwo">
+      <p>KOM ‧ Content</p>
+      <ul>
+        <li><span></span><a href=“#”>Blog</a></li>
+        <li><span></span><a href=“#”>Preguntas Frecuentes</a></li>
+      </ul>
+    </div>
+    <div class="listfoothree">
+      <p>KOM ‧ Web Desing</p>
+      <ul>
+        <li><span></span><a href=“#”>Páginas Web</a></li>
+        <li><span></span><a href=“#”>Landing Pages</a></li>
+        <li><span></span><a href=“#”>Tiendas Virtuales</a></li>
+        <li><span></span><a href=“#”>Rediseño de Paginas Web</a></li>
+        <li><span></span><a href=“#”>Paginas Web MOdulares</a></li>
+        <li><span></span><a href=“#”>Consultor SEO</a></li>
+        <li><span></span><a href=“#”>Cotizador Online</a></li>
+      </ul>
+    </div>
+    <div class="listfoofour">
+      <p>KOM ‧ Perú</p>
+      <ul>
+        <li><span></span><a href="mailto:fdsjfdjs@gmail.com">hola@kom.pe</a></li>
+        <li><span></span><a href="telf:+154654">Oficina (01) 322-2789</a></li>
+        <li><span></span><a href="#">Whatsapp +(51) 923-222-223</a> </li>
+        <li><span></span><a href="linkdegooglemaps">Calle Horacio Urteaga 502</a></li>
+        <li><span></span><a href="linkdegooglemaps">Jesús María 15072 - Lima</a></li>
+        <li><span></span><a href="#">Lunes - Sábado 08:00-20:00</a> </li>
+        <a href="#"></a>
+      </ul>
+      <a class="lastafoo" href="#"></a>
+    </div>
+  </div>
+
+  <div class="ultimatediv">
+    <div class="lasta">
+      <a href="#"><span></span>(+51) 923-222-223</a>
+      <a href="#"><span></span>(+51) 1-322-2789</a>
+    </div>
+    <div class="lastlogo">
+      <div class="lastlogoson"></div>
+      <p>♡ Hecho en Perú</p>
+      <p>© 2017 - 2023</p>
+    </div>
+  </div>
+
+</footer>
---
+++ KOM/estructura/header.php
@@ -0,0 +1,129 @@
+<!DOCTYPE html>
+<html lang="es">
+  <head>
+    <meta charset="UTF-8">
+    <title>Título de la página</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Descripción de la página">
+    <link rel="stylesheet" href="./css/header.css">
+    <link rel="stylesheet" href="./css/main.css">
+    <link rel="stylesheet" href="./css/info.css">
+    <link rel="stylesheet" href="./css/servicios.css">
+    <link rel="stylesheet" href="./css/planes.css">
+    <link rel="stylesheet" href="./css/perso.css">
+    <link rel="stylesheet" href="./css/mininfo.css">
+    <link rel="stylesheet" href="./css/acordeon.css">
+    <link rel="stylesheet" href="./css/ebook.css">
+    <link rel="stylesheet" href="./css/clients.css">
+    <link rel="stylesheet" href="./css/testi.css">
+    <link rel="stylesheet" href="./css/hyper.css">
+    <link rel="stylesheet" href="./css/contact.css">
+    <link rel="stylesheet" href="./css/footer.css">
+    <link rel="stylesheet" media="(min-width: 465px)" href="./css/tablets.css"/>
+    <link rel="stylesheet" media="(min-width: 1024px)" href="./css/laptop.css"/>
+  </head>
+  <div class="redesabs">
+    <a href="#"></a>
+    <a href="#"></a>
+    <a href="#"></a>
+    <a href="#"></a>
+    <a href="#"></a>
+    <a href="#"></a>
+  </div>
+  <?php require('./estructura/promotion.php')?>
+
+<header>
+  <div class="trueheader">
+          <div class="logoheader"></div>
+          <div class="buttonshead">
+  <a href="#">inicio</a>
+  <div class="dropdown">
+    <a href="#">Servicios +</a>
+    <div class="dropdown-content">
+      <a href="#">Páginas Web</a>
+      <a href="#">Tiendas Virtuales</a>
+      <a href="#">Landing Pages</a>
+      <a href="#">Rediseño de Páginas Web</a>
+      <a href="#">Consultoría SEO</a>
+      <a href="#">Servicios Cloud</a>
+    </div>
+  </div>
+  <a href="#">Portafolio</a>
+  <a href="#">Precios</a>
+  <a href="#">Cursos</a>
+  <a href="#">Blog</a>
+  <a href="#">Contacto</a>
+</div>
+    <label for="openSidebarMenu" class="sidebarIconToggle"></label>
+  </div>
+  <input type="checkbox" class="openSidebarMenu" id="openSidebarMenu">
+
+<div id="sidebarMenu">
+  <div class="logoafter"></div>
+  <div class="slidermenugrid">
+    <div class="logoslidediv">
+      <div class="logohead"></div>
+      <label for="openSidebarMenu" class="innericon"></label>
+    </div>
+    <div class="fourlinks">
+      <a href="#">
+        <span class="home"></span>Home </a>
+      <a href="#">
+        <span class="porta"></span>Portafolio </a>
+      <a href="#">
+        <span class="coti"></span>Cotizador </a>
+      <a href="#">
+        <span class="contact"></span>Contacto </a>
+    </div>
+    <div class="fourcube">
+      <div class="cube1">
+        <a href="#">
+          <span></span>Páginas Web </a>
+      </div>
+      <div class="cube2">
+        <a href="#">
+          <span></span>Tiendas Virtuales </a>
+      </div>
+      <div class="cube3">
+        <a href="#">
+          <span></span>Landing Pages </a>
+      </div>
+      <div class="cube4">
+        <a href="#">
+          <span></span>Google SEO </a>
+      </div>
+    </div>
+    <div class="fourlinkstwo">
+      <a href="#">
+        <span class="blog"></span>Blog </a>
+      <a href="#">
+        <span class="student"></span>Cursos </a>
+      <a href="#">
+        <span class="jobs"></span>KOM jobs </a>
+      <a href="#">
+        <span class="Whatsapp"></span>Whatsapp </a>
+    </div>
+    <div class="socialmedia">
+      <a href="#">
+        <span class="telf"></span>
+      </a>
+      <a href="#">
+        <span class="correo"></span>
+      </a>
+      <a href="#">
+        <span class="fb"></span>
+      </a>
+      <a href="#">
+        <span class="ig"></span>
+      </a>
+      <a href="#">
+        <span class="in"></span>
+      </a>
+      <a href="#">
+        <span class="yt"></span>
+      </a>
+    </div>
+  </div>
+</div>
+</div>
+</header>
